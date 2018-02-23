6a75c1490ce9d47657fc5f0037b634e2d26d5539
Rafał Olejniczak <rafalo235@gmail.com>
2018-02-23 19:20:29 +0100

    parser: begin of request parsing; - utils module; - parser module;

---
+++ inc/uchttpserver.h
@@ -0,0 +1,74 @@
+/*
+ uchttpserver.h
+
+ MIT License
+
+ Copyright (c) 2018 Rafał Olejniczak
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+  Created on: Feb 23, 2018
+      Author: Rafał Olejniczak
+ */
+
+#ifndef UCHTTPSERVER_H_
+#define UCHTTPSERVER_H_
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#define STRING_WITH_LENGTH(x) {x, sizeof(x) - 1}
+
+typedef enum HttpStatusCode
+{
+  HTTP_STATUS_CONTINUE = 100,
+  HTTP_STATUS_OK = 200,
+  HTTP_STATUS_NOT_FOUND = 404
+} tHttpStatusCode;
+
+typedef struct StringWithLength
+{
+  const char *str;
+  unsigned int length;
+} tStringWithLength;
+
+typedef tHttpStatusCode (*tResourceCallback)(void);
+
+typedef struct ResourceEntry
+{
+  const char *name;
+  tResourceCallback callback;
+} tResourceEntry;
+
+/**
+ * \brief Entry point for input stream processing
+ */
+void
+Http_Input(const char * data, unsigned int length);
+
+/**
+ * \brief Search for pattern in given input stream
+ */
+unsigned int
+Utils_SearchPattern(
+    const char *pattern, const char *stream,
+    unsigned int pattenlen, unsigned int streamlen);
+
+#endif /* UCHTTPSERVER_H_ */
---
+++ src/parser.c
@@ -0,0 +1,121 @@
+/*
+ parser.c
+
+ MIT License
+
+ Copyright (c) 2018 Rafał Olejniczak
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+  Created on: Feb 23, 2018
+      Author: Rafał Olejniczak
+ */
+
+#include "uchttpserver.h"
+
+typedef unsigned int (*tParserState)(const char * data, unsigned int length);
+
+/* Parse request states declarations */
+static unsigned int ParseMethodState(const char * data, unsigned int length);
+
+tStringWithLength methods[] =
+    {
+	 STRING_WITH_LENGTH("GET"), /* Most commonly used */
+	 STRING_WITH_LENGTH("POST"),
+	 STRING_WITH_LENGTH("OPTIONS"),
+	 STRING_WITH_LENGTH("HEAD"),
+	 STRING_WITH_LENGTH("PUT"),
+	 STRING_WITH_LENGTH("DELETE"),
+	 STRING_WITH_LENGTH("TRACE"),
+	 STRING_WITH_LENGTH("CONNECT")
+    };
+
+static tParserState sState = ParseMethodState;
+
+void Http_Input(const char * data, unsigned int length)
+{
+  sState(data, length);
+}
+
+/* Parse request states definitions */
+static unsigned int ParseMethodState(const char * data, unsigned int length)
+{
+  static const char *found = NULL;
+  static unsigned int tofoundlen = 0U;
+  unsigned int i;
+  unsigned int size = sizeof(methods)/sizeof(methods[0]);
+  unsigned int parsed = 0;
+
+  /* Pattern already started on stream,
+   * check if it's continued */
+  if (found != NULL)
+    {
+      unsigned int counted =
+	  Utils_SearchPattern(found, data, tofoundlen, length);
+      if (counted == tofoundlen)
+	{
+	  /* Match! */
+	  parsed += tofoundlen;
+	}
+      else if (counted == length)
+	{
+	  /* Stream shorter than pattern,
+	   * continue searching on next input */
+	  found += length;
+	  tofoundlen -= length;
+	  parsed += length;
+	}
+      else
+	{
+	  /* Pattern broken,
+	   * continue searching other patterns */
+	  found = NULL;
+	  tofoundlen = 0U;
+	  /* Leave <parsed> unchanged */
+	}
+    }
+
+  while (length)
+    {
+      /* Linear search - not a lot of elements */
+      for (i = 0; i < size; ++i)
+	{
+	  unsigned int counted =
+	      Utils_SearchPattern(methods[i].str, data, methods[i].length, length);
+	  if (counted == methods[i].length)
+	    {
+	      /* Match! */
+	      parsed += methods[i].length;
+	      break;
+	    }
+	  else if (counted == length)
+	    {
+	      /* To be continued */
+	      /* FIXME stream ends with 'P' */
+	      found = methods[i].str + length;
+	      tofoundlen = methods[i].length - length;
+
+	      /* Exit outer loop */
+	      length = 0;
+	      break;
+	    }
+	}
+    }
+  return parsed;
+}
