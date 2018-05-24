642b023b4ad5eaf3c67288bd51aff5432a509740
Rafał Olejniczak <rafalo235@gmail.com>
2018-05-24 17:41:42 +0200

    project: uchttpoption file template; - added template file with project options;

---
+++ template/uchttpoption.h
@@ -0,0 +1,49 @@
+/*
+ uchttpoption.h
+
+ MIT License
+
+ Copyright (c) 2018 Rafał Olejniczak
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+  Created on: May 21, 2018
+      Author: Rafał Olejniczak
+ */
+
+#ifndef UCHTTPOPTION_H_
+#define UCHTTPOPTION_H_
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#ifndef HTTP_BUFFER_LENGTH
+#define HTTP_BUFFER_LENGTH (256)
+#endif
+
+#ifndef HTTP_PARAMETERS_BUFFER_LENGTH
+#define HTTP_PARAMETERS_BUFFER_LENGTH (640)
+#endif
+
+#ifndef HTTP_PARAMETERS_MAX
+#define HTTP_PARAMETERS_MAX (16)
+#endif
+
+#endif /* UCHTTPOPTION_H_ */
